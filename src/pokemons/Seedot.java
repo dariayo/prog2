967b71664cb57e05cb2ed2cf87dc5e13f01de11d
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:12:16 +0300

    Update Seedot.java

--- src/pokemons/Seedot.java
+++ src/pokemons/Seedot.java
@@ -4,9 +4,9 @@ import moves.*;
 import ru.ifmo.se.pokemon.*;
 
 public class Seedot extends Pokemon {
-    public Seedot (String name, int level){
-        super(name,level);
-        setStats(40,40,50,30,30,30);
+    public Seedot(String name, int level) {
+        super(name, level);
+        setStats(40, 40, 50, 30, 30, 30);
         setType(Type.GRASS);
         setMove(new DoubleTeam(), new SwordsDance());
     }
