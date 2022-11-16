2b0349c0c7b1c7d1b102c0ec5de754136fb79840
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:05:34 +0300

    Update DoubleTeam.java

--- src/moves/DoubleTeam.java
+++ src/moves/DoubleTeam.java
@@ -2,16 +2,19 @@ package moves;
 
 import ru.ifmo.se.pokemon.*;
 
+
 public class DoubleTeam extends StatusMove {
-    public DoubleTeam(){
-        super(Type.NORMAL,0,0);
+    public DoubleTeam() {
+        super(Type.NORMAL, 0, 0);
     }
+
     @Override
-    protected void applySelfEffects(Pokemon p){
-        p.setMod(Stat.EVASION,1);
+    protected void applySelfEffects(Pokemon p) {
+        p.setMod(Stat.EVASION, 1);
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Double Team";
     }
 }
