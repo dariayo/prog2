fd7f3f20caf79e78531bd634e958cb7524218d9b
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 18:41:02 +0300

    Update Nuzleaf.java

--- src/pokemons/Nuzleaf.java
+++ src/pokemons/Nuzleaf.java
@@ -3,7 +3,7 @@ package pokemons;
 import moves.*;
 import ru.ifmo.se.pokemon.*;
 
-public class Nuzleaf extends Pokemon {
+public class Nuzleaf extends Seedot {
     public Nuzleaf (String name, int level){
         super(name,level);
         setStats(70,70,40,60,40,60);
