4add146e3a438de589537e4c0ece27e2e6402d2f
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:07:10 +0300

    Update LeafStorm.java

--- src/moves/LeafStorm.java
+++ src/moves/LeafStorm.java
@@ -3,15 +3,17 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class LeafStorm extends SpecialMove {
-    public LeafStorm(){
-        super(Type.GRASS,130,90);
+    public LeafStorm() {
+        super(Type.GRASS, 130, 90);
     }
+
     @Override
-    protected void applySelfEffects(Pokemon p){
-        p.setMod(Stat.SPECIAL_ATTACK,-2);
+    protected void applySelfEffects(Pokemon p) {
+        p.setMod(Stat.SPECIAL_ATTACK, -2);
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Leaf Storm";
     }
 }
