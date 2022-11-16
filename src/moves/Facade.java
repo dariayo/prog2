d1d272f19d9b534b84eabc479ba5f2169e2201cf
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:06:27 +0300

    Update Facade.java

--- src/moves/Facade.java
+++ src/moves/Facade.java
@@ -1,21 +1,24 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
+
 public class Facade extends PhysicalMove {
-    public Facade(){
-        super(Type.NORMAL,70,100);
+    public Facade() {
+        super(Type.NORMAL, 70, 100);
     }
+
     @Override
-    protected void applyOppDamage(Pokemon def,double damage){
-        Status status=def.getCondition();
-        if (status.equals(Status.BURN)||status.equals(Status.POISON)||status.equals(Status.PARALYZE)){
-            super.applyOppDamage(def,damage*2);
-        }
-        else {
-            super.applyOppDamage(def,damage);
+    protected void applyOppDamage(Pokemon def, double damage) {
+        Status status = def.getCondition();
+        if (status.equals(Status.BURN) || status.equals(Status.POISON) || status.equals(Status.PARALYZE)) {
+            super.applyOppDamage(def, damage * 2);
+        } else {
+            super.applyOppDamage(def, damage);
         }
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Facade";
     }
 }
