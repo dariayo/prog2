acbe90c45f3063f8b59650b25e16d8feaa4f8373
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:06:45 +0300

    Update Harden.java

--- src/moves/Harden.java
+++ src/moves/Harden.java
@@ -3,15 +3,17 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class Harden extends StatusMove {
-    public Harden(){
-        super(Type.NORMAL,0,0);
+    public Harden() {
+        super(Type.NORMAL, 0, 0);
     }
+
     @Override
-    protected void applySelfEffects(Pokemon p){
-        p.setMod(Stat.DEFENSE,1);
+    protected void applySelfEffects(Pokemon p) {
+        p.setMod(Stat.DEFENSE, 1);
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Harden";
     }
 }
