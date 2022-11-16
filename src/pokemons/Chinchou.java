5b853484d82942f4a44dcbf2b630af2c4fc21521
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:10:06 +0300

    Update Chinchou.java

--- src/pokemons/Chinchou.java
+++ src/pokemons/Chinchou.java
@@ -4,10 +4,11 @@ import moves.*;
 import ru.ifmo.se.pokemon.*;
 
 public class Chinchou extends Pokemon {
-    public Chinchou (String name, int level){
-        super(name,level);
-        setStats(75,38,38,56,56,67);
-        setType(Type.WATER,Type.ELECTRIC);
-        setMove(new Rest(), new BubbleBeam(),new TakeDown());
+    public Chinchou(String name, int level) {
+        super(name, level);
+        setStats(75, 38, 38, 56, 56, 67);
+        setType(Type.WATER, Type.ELECTRIC);
+        setMove(new Rest(), new BubbleBeam(), new TakeDown());
     }
 }
+
