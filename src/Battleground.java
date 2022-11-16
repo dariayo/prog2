9ef424d7141de70b12f18171de2e465a52cce048
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:01:34 +0300

    Update Battleground.java

--- src/Battleground.java
+++ src/Battleground.java
@@ -5,13 +5,12 @@ public class Battleground {
 
     public static void main(String[] args) {
         Battle fight = new Battle();
-        fight.addAlly(new Chinchou("Apple",2));
+        fight.addAlly(new Chinchou("Apple",40));
         fight.addAlly(new Komala("Banana",2));
         fight.addAlly(new Seedot("Peach",2));
-        fight.addFoe(new Nuzleaf("Potato",2));
-        fight.addFoe(new Lanturn("Carrot",2));
-        fight.addFoe(new Shiftry("Tomato",2));
+        fight.addFoe(new Nuzleaf("Potato",1));
+        fight.addFoe(new Lanturn("Carrot",48));
+        fight.addFoe(new Shiftry("Tomato",1));
         fight.go();
     }
 }
-
