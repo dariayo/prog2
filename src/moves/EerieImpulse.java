48c206f42a6a12dce353cab771904bf4a9cdaf33
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:06:00 +0300

    Update EerieImpulse.java

--- src/moves/EerieImpulse.java
+++ src/moves/EerieImpulse.java
@@ -3,15 +3,17 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class EerieImpulse extends StatusMove {
-    public EerieImpulse(){
-        super(Type.ELECTRIC,0,100);
+    public EerieImpulse() {
+        super(Type.ELECTRIC, 0, 100);
     }
+
     @Override
-    protected void applyOppEffects(Pokemon p){
-        p.setMod(Stat.SPECIAL_ATTACK,-2);
+    protected void applyOppEffects(Pokemon p) {
+        p.setMod(Stat.SPECIAL_ATTACK, -2);
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Eerie Impulse";
     }
 }
