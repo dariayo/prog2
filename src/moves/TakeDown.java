3565558400fd2184191a4f9a60ea323a54338bd3
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:08:45 +0300

    Update TakeDown.java

--- src/moves/TakeDown.java
+++ src/moves/TakeDown.java
@@ -4,17 +4,18 @@ import ru.ifmo.se.pokemon.*;
 
 
 public class TakeDown extends PhysicalMove {
-    public TakeDown(){
-        super(Type.NORMAL,90,85);
+    public TakeDown() {
+        super(Type.NORMAL, 90, 85);
     }
+
     @Override
     protected void applySelfDamage(Pokemon att, double damage) {
-        //Метод, применяющий вычисленное повреждение к атакующему покемону.
-        att.setMod(Stat.HP, (int) Math.round(damage/4));
+        att.setMod(Stat.HP, (int) Math.round(damage / 4));
     }
+
     @Override
-    protected String describe(){
-        return" использует Take Down";
+    protected String describe() {
+        return " использует Take Down";
     }
 
 }
