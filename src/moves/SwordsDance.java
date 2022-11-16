de6b693dc09c27a1ba29a2e93eb99e4b87b48f4b
dariayo <90570083+dariayo@users.noreply.github.com>
2022-11-16 19:08:17 +0300

    Update SwordsDance.java

--- src/moves/SwordsDance.java
+++ src/moves/SwordsDance.java
@@ -1,15 +1,19 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
+
 public class SwordsDance extends StatusMove {
-    public SwordsDance(){
-        super(Type.NORMAL,0,0);
+    public SwordsDance() {
+        super(Type.NORMAL, 0, 0);
     }
+
     @Override
-    protected void applySelfEffects(Pokemon p){
-        p.setMod(Stat.ATTACK,2);
+    protected void applySelfEffects(Pokemon p) {
+        p.setMod(Stat.ATTACK, 2);
     }
+
     @Override
-    protected String describe(){
+    protected String describe() {
         return "использует Swords Dance";
     }
 }
